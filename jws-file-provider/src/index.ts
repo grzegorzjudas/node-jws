0af8b21052c87ae34af686e17045d03dcba3095d
Grzegorz Judas <grzegorz.judas@gmail.com>
2020-11-04 16:22:00 +0100

    Allow not passing keys to file provider

--- jws-file-provider/src/index.ts
+++ jws-file-provider/src/index.ts
@@ -3,14 +3,18 @@ import { JWS, JWK } from 'jose';
 import { KeyProvider, JWTClaims, JWTHeader } from 'node-jws';
 
 export default function FileJwtProvider (privateKeyPath: string, publicKeyPath: string): KeyProvider {
-    const privateKey = JWK.asKey(fs.readFileSync(privateKeyPath));
-    const publicKey = JWK.asKey(fs.readFileSync(publicKeyPath));
+    const privateKey = privateKeyPath ? JWK.asKey(fs.readFileSync(privateKeyPath)) : null;
+    const publicKey = publicKeyPath ? JWK.asKey(fs.readFileSync(publicKeyPath)) : null;
 
     return {
         sign: async (claims: JWTClaims, header: JWTHeader) => {
+            if (!privateKey) throw new Error('Cannot sign JWS - private key not provided.');
+
             return JWS.sign(claims, privateKey, header);
         },
         valid: async (raw: string) => {
+            if (!publicKey) throw new Error('Cannot verify JWS - public key not provided.');
+
             try {
                 JWS.verify(raw, publicKey);
 
